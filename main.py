5c3db9259fd6321cf24c3367224070649bd29c37
David <pogosian.david2018@yandex.ru>
2023-11-21 21:59:00 +0700

    Функции распознавания объектов на фото и видео

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 #Импорт библиотек
-from imageai.Detection import ObjectDetection
-import cv2
+import ultralytics
+from ultralytics import YOLO
+import cv2 as cv
+import torch
 
-Detector = ObjectDetection()
+print(torch.cuda.is_available())
+
+#Загрузка модели YOLOv8
+model = YOLO('yolov8m-oiv7.pt')
+
+#Обработка фото
+def image_processing(image):
+    return model.predict(image, show = True)
+
+#Обработка видео
+def video_processing(video):
+    return model.predict(video, show = True)
+
